28093db9066988019fdeeb8d020b2897edb2e2f7
Robert Pajak <pellared@hotmail.com>
2020-06-12 08:24:18 +0200

    Remove release make target

--- install.sh
+++ install.sh
@@ -6,6 +6,3 @@ cd && GO111MODULE=on go get golang.org/x/tools/cmd/goimports@master && cd - > /d
 
 echo Installing golangci-lint...
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$(go env GOPATH)/bin" v1.27.0
-
-echo Installing GoReleaser...
-curl -sSfL https://install.goreleaser.com/github.com/goreleaser/goreleaser.sh | sh -s -- -b "$(go env GOPATH)/bin" v0.137.0
