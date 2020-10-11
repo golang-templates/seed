299b85acdcb2e36fcac20759b20d34f1bc65ae54
Robert Pająk <pellared@hotmail.com>
2020-10-11 10:45:02 +0200

    Use gofumpt and gofumports (#71)

--- tools.go
+++ tools.go
@@ -9,5 +9,5 @@ package tools
 import (
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/goreleaser/goreleaser"
-	_ "golang.org/x/tools/cmd/goimports"
+	_ "mvdan.cc/gofumpt/gofumports"
 )
